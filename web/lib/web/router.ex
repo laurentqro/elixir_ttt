10a01a271210c7296ada83ee721c791e794d8a30
Laurent Curau <laurentcurau@gmail.com>
2018-07-31 17:07:12 +0100

    Refactor to pipe all the things

--- web/lib/web/router.ex
+++ web/lib/web/router.ex
@@ -6,15 +6,26 @@ defmodule Web.Router do
   plug  :dispatch
 
   get "/play" do
-    play(conn)
+    encoded_new_game = Tictactoe.new_game()
+                       |> Poison.encode!
+                       |> Base.encode64
+
+    conn |> redirect_to("/play/#{encoded_new_game}")
   end
 
   get "/play/:encoded_game" do
-    get_encoded_game(conn, encoded_game)
+    conn |> send_resp(200, encoded_game |> parse_encoded_game)
   end
 
   get "/play/:encoded_game_state/move/:move" do
-    play_move(conn, encoded_game_state, move)
+    encoded_new_game_state = encoded_game_state
+                             |> parse_encoded_game
+                             |> Poison.decode!(as: %Tictactoe.Game{})
+                             |> Tictactoe.make_move(move |> parse_move)
+                             |> Poison.encode!
+                             |> Base.encode64
+
+    conn |> redirect_to("/play/#{encoded_new_game_state}")
   end
 
   match _ do
@@ -27,23 +38,13 @@ defmodule Web.Router do
     |> resp(301, message)
   end
 
-  def play(conn) do
-    encoded_new_game = Tictactoe.new_game()
-    |> Poison.encode!
-    |> Base.encode64
-    conn |> redirect_to("/play/#{encoded_new_game}")
+  defp parse_encoded_game(encoded_game) do
+    { :ok, decoded_game } = encoded_game |> Base.decode64
+    decoded_game
   end
 
-  def get_encoded_game(conn, encoded_game) do
-    { :ok, game } = encoded_game |> Base.decode64
-    conn |> send_resp(200, game)
-  end
-
-  def play_move(conn, encoded_game_state, move) do
-    { :ok, decoded_game_state } = encoded_game_state |> Base.decode64
-    { move, _rem } =  Integer.parse(move)
-    encoded_new_game_state = Tictactoe.make_move(decoded_game_state |> Poison.decode!(as: %Tictactoe.Game{}), move) |> Poison.encode! |> Base.encode64
-
-    conn |> redirect_to("/play/#{encoded_new_game_state}")
+  defp parse_move(move) do
+    { move, _rem } =  move |> Integer.parse
+    move
   end
 end
