b643c78f190ddbcd54fb2229f47620b1fbba23f6
Laurent Curau <laurentcurau@gmail.com>
2018-09-04 17:26:31 +0100

    Speed up computation

--- tictactoe/lib/tictactoe/player/computer.ex
+++ tictactoe/lib/tictactoe/player/computer.ex
@@ -4,10 +4,24 @@ defmodule Player.Computer do
   alias Tictactoe.Game
 
   def pick_move(game) do
+    if available_moves_count(game) > 7 do
+      game |> pick_random_move
+    else
+      game |> pick_best_move
+    end
+  end
+
+  defp pick_best_move(game) do
     %{ move: move, score: _score } = game
                                      |> scored_moves
                                      |> Enum.max_by(& &1.score)
     move
+  end
+
+  defp pick_random_move(game) do
+    game
+    |> Tictactoe.Game.available_moves
+    |> Enum.random
   end
 
   defp scored_moves(game) do
